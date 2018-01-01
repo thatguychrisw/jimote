91f6da00b9383b7ae32d9cef9271a5ed76385df0
thatguychrisw <thatguychrisw@gmail.com>
2018-01-01 10:54:49 -0500

    Add Jira client and login / listProjects logic
    - Add jira-client npm package.
    - Update configWindow to fetch projects via the credentials provided.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import {Menu, shell, app, BrowserWindow, ipcMain} from 'electron';
+import {Menu, shell, app, globalShortcut, screen, BrowserWindow, ipcMain} from 'electron';
 import installExtension, {VUEJS_DEVTOOLS} from 'electron-devtools-installer';
 import {enableLiveReload} from 'electron-compile';
 import ElectronStore from 'electron-store';
 import DefaultMenu from 'electron-default-menu';
 
-let store = new ElectronStore();
+const store = new ElectronStore();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -12,7 +12,12 @@ let mainWindow;
 
 const isDevMode = process.execPath.match(/[\\/]electron/);
 
-if (isDevMode) enableLiveReload();
+if (isDevMode) {
+    enableLiveReload();
+
+    // Reset the jira configuration each load for testing
+    store.set('jira', undefined);
+}
 
 // Remove the app from the dock
 // app.dock.hide();
@@ -33,16 +38,10 @@ const createWindow = async () => {
         backgroundColor: 'white'
     });
 
+    const actionBar = createAppBar();
+
     // Load the application
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-    ipcMain.on('app-search', () => {
-        mainWindow.setSize(appWidth, appHeight + 300, false);
-    });
-
-    ipcMain.on('app-hide', () => {
-        mainWindow.hide();
-    });
+    mainWindow.loadURL(`file://${__dirname}/configWindow.html`);
 
     // Emitted when the window is closed.
     mainWindow.on('closed', () => {
@@ -58,10 +57,13 @@ const createWindow = async () => {
             mainWindow.show();
         }
 
-        // Actionbar shortcut
-        // globalShortcut.register('Command+J', () => {
-        //     mainWindow.show();
-        // });
+        // Register the global shortcut for the JIRA action bar
+        globalShortcut.register('Command+J', () => {
+            const jiraConfig = store.get('jira');
+            if (jiraConfig !== undefined) {
+                actionBar.show();
+            }
+        });
     });
 
     if (isDevMode) {
@@ -93,3 +95,37 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+const createAppBar = () => {
+
+    const appWidth = 800, appHeight = 74;
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize;
+
+    console.log('width', width, 'height', height);
+
+    const
+        appX = Number((width * .5) - (appWidth * .5)),
+        appY = Number(((height * .5) - (appHeight * .5)) * .5);
+
+    console.log('appX', appX, 'appY', appY);
+
+    const actionBar = new BrowserWindow({
+        width: appWidth,
+        height: appHeight,
+        show: false,
+        frame: false,
+        resizable: false,
+        backgroundColor: 'white'
+    });
+
+    actionBar.loadURL(`file://${__dirname}/actionBar.html`);
+
+    ipcMain.on('app-search', () => {
+        actionBar.setSize(appWidth, appHeight + 300, false);
+    });
+
+    ipcMain.on('app-hide', () => {
+        actionBar.hide();
+    });
+
+    return actionBar;
+};
--- src/configWindow.vue
+++ src/configWindow.vue
@@ -16,7 +16,10 @@
                             <v-layout row wrap>
                                 <v-flex xs4>
                                     <div class="info">
-                                        Your JIRA credentials are stored in your OSX keychain.
+                                        <p>Welcome!</p>
+                                        <p>In order to get started using Jimote please provide your JIRA credentials
+                                            so we can access your projects.</p>
+                                        <p>Your credentials are stored in the osx keychain.</p>
                                     </div>
                                 </v-flex>
 
@@ -25,17 +28,20 @@
                                         <v-form v-model="valid" ref="form" lazy-validation>
                                             <v-text-field
                                                     label="JIRA Board URL"
-                                                    v-model="screen.jira.url"
+                                                    v-model="screen.jira.url.value"
+                                                    :rules="screen.jira.url.rules"
                                                     required
                                             ></v-text-field>
                                             <v-text-field
                                                     label="User Name"
-                                                    v-model="screen.jira.userName"
+                                                    v-model="screen.jira.userName.value"
+                                                    :rules="screen.jira.userName.rules"
                                                     required
                                             ></v-text-field>
                                             <v-text-field
                                                     label="Password"
-                                                    v-model="screen.jira.password"
+                                                    v-model="screen.jira.password.value"
+                                                    :rules="screen.jira.password.rules"
                                                     required
                                             ></v-text-field>
 
@@ -58,17 +64,20 @@
 
                                 <v-flex xs8>
                                     <div class="form">
-                                        <v-form v-model="valid" ref="form" lazy-validation>
+                                        <v-form v-model="valid" ref="projectForm" lazy-validation>
                                             <v-select
-                                                    v-bind:items="[{text: 'Charge Manager'}, {text: 'Portal'}]"
-                                                    v-model="screen.projects.names"
+                                                    :items="screen.project.jira.projects"
+                                                    v-model="screen.project.name"
+                                                    item-text="name"
+                                                    item-value="key"
                                                     label="Select"
                                                     single-line
                                                     bottom
                                             ></v-select>
 
                                             <div class="actions">
-                                                <v-btn color="primary" @click.native="screenIdxSelected = 3">Continue</v-btn>
+                                                <v-btn color="primary" @click.native="screenIdxSelected = 3">Continue
+                                                </v-btn>
                                                 <v-btn flat @click.native="screenIdxSelected = 1">Back</v-btn>
                                             </div>
                                         </v-form>
@@ -81,9 +90,7 @@
                             <v-layout row wrap>
                                 <v-flex xs12>
                                     <div class="info">
-                                        DONE!
-
-                                        <v-btn color="primary" @click.native="screenIdxSelected = 1">Continue</v-btn>
+                                        READY TO USE
                                     </div>
                                 </v-flex>
                             </v-layout>
@@ -96,6 +103,10 @@
 </template>
 
 <script>
+    import ElectronStore from 'electron-store';
+    import JiraApi from 'jira-client';
+
+    const store = new ElectronStore();
 
     export default {
         name: 'configWindow',
@@ -105,13 +116,32 @@
 
                 screen: {
                     jira: {
-                        url: null,
-                        userName: null,
-                        password: null,
+                        url: {
+                            value: null,
+                            rules: [
+                                (v) => !!v || 'JIRA project url is required',
+                            ],
+                        },
+                        userName: {
+                            value: null,
+                            rules: [
+                                (v) => !!v || 'user name is required',
+                            ],
+                        },
+                        password: {
+                            value: null,
+                            rules: [
+                                (v) => !!v || 'password is required',
+                            ],
+                        },
                     },
 
-                    projects: {
-                        names: [],
+                    project: {
+                        jira: {
+                            projects: []
+                        },
+
+                        name: null,
                     }
                 },
             }
@@ -120,17 +150,28 @@
         methods: {
             submit() {
                 if (this.$refs.form.validate()) {
+                    store.set('jira.url', this.screen.jira.url.value);
+                    store.set('jira.userName', this.screen.jira.userName.value);
+                    store.set('jira.password', this.screen.jira.password.value);
 
-                    this.screenIdxSelected = 2;
+                    const jira = new JiraApi({
+                        host: this.screen.jira.url.value,
+                        username: this.screen.jira.userName.value,
+                        password: this.screen.jira.password.value,
+                    });
+
+                    jira.listProjects().then((projects) => {
+                        this.screen.project.jira.projects = projects.map(project => ({
+                            key: project.key,
+                            name: project.name
+                        }));
+
+                        this.screenIdxSelected = 2;
+                    });
                 }
             },
-            clear() {
-                this.$refs.form.reset()
-            }
         },
 
-        mounted() {
-        }
     }
 </script>
 
